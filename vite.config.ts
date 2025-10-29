0bb2b159137d294a5838a742da862f2734ec3d03
Debyani Ghosh <ghoshdebyani@gmail.com>
2025-10-29 21:32:10 +0530

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: '/Book-nest/', 
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
